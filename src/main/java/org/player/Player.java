d9c93f038a45b119ed3839555e8923cef45f2df8
CommanderSpinner <klaus.kutulu@gmail.com>
2025-01-16 18:32:24 +0100

    Update Player.java

--- src/main/java/org/player/Player.java
+++ src/main/java/org/player/Player.java
@@ -2,7 +2,11 @@ package org.player;
 
 public class Player {
     char symbol;
+    static char fields[];
     public Player(char symbol){
         this.symbol = symbol;
     }
+
+    static private boolean check
+
 }
