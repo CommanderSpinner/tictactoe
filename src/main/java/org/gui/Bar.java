5311e98575caa05b2db95c852c5d85d412bf94db
CommanderSpinner <klaus.kutulu@gmail.com>
2025-01-18 11:58:06 +0100

    fixxed menu bar

--- src/main/java/org/gui/Bar.java
+++ src/main/java/org/gui/Bar.java
@@ -5,23 +5,30 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Bar extends JMenuBar implements ActionListener {
-    JMenu[] items;
-    public Bar(){
-        items = new JMenu[2];
-        items[0] = new JMenu("Game");
-        items[1] = new JMenu("Stats");
+    JMenu menu;
+    JMenuItem[] menuItems;
 
-        items[0].addActionListener(this);
-        items[1].addActionListener(this);
+    public Bar() {
+        menu = new JMenu("panels");
+        menuItems = new JMenuItem[2];
 
-        this.setVisible(true);
+        menuItems[0] = new JMenuItem("Game");
+        menuItems[1] = new JMenuItem("Stats");
+
+        menuItems[0].addActionListener(this);
+        menuItems[1].addActionListener(this);
+
+        menu.add(menuItems[0]); // Add the JMenuItem to the Game menu
+        menu.add(menuItems[1]); // Add the JMenuItem to the Stats menu
+
+        this.add(menu); // Add the Game menu to the menu bar
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == items[0]) {
+        if (e.getSource() == menuItems[0]) {
             System.out.println("Game menu clicked");
-        } else if (e.getSource() == items[1]) {
+        } else if (e.getSource() == menuItems[1]) {
             System.out.println("Stats menu clicked");
         }
     }
