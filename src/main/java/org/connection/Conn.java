f9cd11f99eb2904ddec57d0b35cc6010ce37149c
CommanderSpinner <klaus.kutulu@gmail.com>
2025-01-19 19:11:46 +0100

    simplified db and started coding player class for db

--- src/main/java/org/connection/Conn.java
+++ src/main/java/org/connection/Conn.java
@@ -1,4 +1,5 @@
 package org.connection;
 
 public class Conn {
+
 }
--- src/main/java/org/connection/PlayerRecord.java
+++ src/main/java/org/connection/PlayerRecord.java
@@ -1,4 +1,18 @@
 package org.connection;
 
+import org.player.Player;
+
 public class PlayerRecord {
+    char symbol;
+    int score;
+    Conn c;
+    Player p;
+
+    public void synchDB(){
+    }
+
+    public PlayerRecord(Conn c, Player p){
+        this.c = c;
+        this.p = p;
+    }
 }
